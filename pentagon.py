0ed58442e795cbf23638da7efe61fb5500165885
jack-dangelo <54954654+jack-dangelo@users.noreply.github.com>
2020-01-28 14:51:21 -0500

    Create pentagon.py

---
+++ pentagon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Jack D'Angelo
+# Created on: September 2019
+# This program calculates the perimeter of a pentagon
+
+
+def main():
+    a = int(input("Enter the length of a side: "))
+
+    perimeter = 5 * a
+
+    print("Pentagon perimeter {}cm" .format(perimeter))
+
+
+if __name__ == "__main__":
+    main()
